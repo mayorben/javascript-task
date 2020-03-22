1cd21768c51ac23e8e58bf24fc46289bb07d4ff1
mayorben <bennytechno1@gmail.com>
2020-03-22 18:05:31 +0100

    jstask

---
+++ script.js
@@ -0,0 +1,16 @@
+let name = 'BENSON MAYOWA';
+let Courses = ['HTML', 'CSS', 'JAVSCRIPT', 'NodeJs'];
+
+console.log(name);
+console.log(Courses.toLocaleString());
+
+    let y;
+    for (y=1; y<=200; y++) {
+        if (Courses.length % 2 ===0){
+        y % 2 ===0 ? console.log(y) : '';
+     }
+        else y % 2 ===1 ? console.log(y) : '';
+    };
+
+
+
